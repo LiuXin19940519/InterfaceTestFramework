6e7c8366187f4f5907ccbef521a86e9f45b00708
MyGitHub <18565372743@163.com>
2018-03-29 22:23:02 +0800

    添加了GetXMLConfig类，完成了配置文档的读取

--- src/main/java/com/liuxin/getConfig/GetXMLConfig.java
+++ src/main/java/com/liuxin/getConfig/GetXMLConfig.java
@@ -1,4 +1,38 @@
 package com.liuxin.getConfig;
 
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
+import java.util.List;
+
 public class GetXMLConfig {
+    /**
+     * 获取文件的document对象，然后获取对应的根节点
+     * @author chenleixing
+     */
+    public static void main(String [] args){
+        try {
+            SAXReader saxReader = new SAXReader();
+            File xmlFile = new File("E:\\JAVA\\InterfaceTestFramework\\src\\main\\resources\\config.xml");
+            Document document = saxReader.read(xmlFile);
+            Element root = document.getRootElement();
+            List<Element> va = root.elements("user");
+            //List<Node> user = root.selectNodes("user");
+
+            for (Element n :va){
+                Attribute a =n.attribute("editor");
+                String c=a.getName();
+                String d = a.getValue();
+                Attribute b =n.attribute("date");
+                System.out.println(a+ "  " +c +"的值为：" + d);
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
